d803a054637e154c9a738bd321061ab831930efb
kyrolloszakaria <76782214+kyrolloszakaria@users.noreply.github.com>
2023-11-27 14:55:58 +0200

    Update draft.py

--- draft.py
+++ draft.py
@@ -1,17 +1,1 @@
-# hello xinyu
-# from xinyu pc
-
-import inspect
-
-def print_function_name():
-    frame = inspect.currentframe()
-    try:
-        caller_name = inspect.getframeinfo(frame.f_back).function
-        print("The name of the calling function is:", caller_name)
-    finally:
-        del frame  # Make sure to clean up the frame
-
-def foo():
-    print_function_name()
-
-foo()
+# here, this one!
